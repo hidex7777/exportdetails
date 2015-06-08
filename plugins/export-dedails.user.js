8a36155f9ed3b232d775b211448bbdaed5a9cf74
hidex7777 <hidex7777@gmail.com>
2015-06-08 23:21:06 +0900

    update specification

--- plugins/export-dedails.user.js
+++ plugins/export-dedails.user.js
@@ -1,24 +1,12 @@
 // ==UserScript==
-// @id             iitc-plugin-export-details@export
-// @name           IITC plugin: Export Details
-// @category       Misc
-// @version        0.0.1.@@DATETIMEVERSION@@
-// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Export info about Portals into local strage. Portal ID(pll), Portal name, level, MODs, Resonators, fuction, shielding, AP Gain.
+// @name           Export Details for IITC
+// @version        0.0.1
+// @description    Export info about Portals into local strage. Portal ID(pll), Portal name, level, MODs, Resonators, fuction, shielding, AP Gain.
+// @namespace      kojunkan.org
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
 // @match          http://www.ingress.com/intel*
 // @grant          none
 // ==/UserScript==
-
-// PLUGIN START ////////////////////////////////////////////////////////
-// use own namespace for plugin
-window.plugin.exportdetails = function() {};
-
-
-window.plugin.exportdetails.setupLink = function() {
-  $('#toolbox').append('<a id="export-details-link">Export</a>');
-}
-
-// PLUGIN END //////////////////////////////////////////////////////////
 
