868ddc5fbc4814cefd8e12b44217ba771a3fb6b3
hidex7777 <hidex7777@gmail.com>
2015-06-26 09:47:01 +0900

    define button visibility

--- plugins/export-dedails.user.js
+++ plugins/export-dedails.user.js
@@ -10,3 +10,42 @@
 // @grant          none
 // ==/UserScript==
 
+//jQuery読み込み
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
+
+//main
+function main() {
+  //$の代わりにjQを使う
+  jQ('body').append(jQ('<div>')
+    .css({
+      position: 'fixed',
+      bottom: '50px',
+      right: '50px',
+      width: '70px',
+      height: '70px',
+      backgroundColor: '#004F4A',
+      zIndex: '9999',
+      borderRadius: '50%'
+    })
+    .append(jQ('<div>', { text: 'E'})
+      .css({
+        fontSize: '3em',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#47D9CB'
+      })
+    )
+  );
+}
+
+//load jQuery and execute main function
+addJQuery(main);
